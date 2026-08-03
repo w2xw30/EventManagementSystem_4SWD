bfc80d5d8e780e5886d121a2cf655069966c1797
BisAce2x <bishesh10subedi@gmail.com>
2026-08-03 12:35:46 +0545

    Force client rebuild

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { ToastProvider } from "./context/ToastContext.jsx";
 import "./global.css";
-
+// Hello this is a comment
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ToastProvider>
